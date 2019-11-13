f71ed31adadcdd93ab5aacd78c93ab87f4ef8ee7
Mike Hartington <mikehartington@gmail.com>
2019-11-13 13:47:24 -0800

    remove toast

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,11 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { register } from './serviceWorker';
 import store from './store';
-import {IonToast} from '@ionic/react';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <App />
-    <IonToast isOpen={true} message="Your settings have been saved." />
   </Provider>,
   rootElement
 );
@@ -18,7 +16,5 @@ ReactDOM.render(
 register({
   onUpdate: async (registration: ServiceWorkerRegistration) => {
     await registration.update();
-    console.log(registration);
-    console.log('new version');
   }
 });
