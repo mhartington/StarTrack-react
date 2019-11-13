d74e5d1304ca9aa1e4e322f5238a7261208c7bf9
Mike Hartington <mikehartington@gmail.com>
2019-11-13 13:38:10 -0800

    updates

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
+import { Provider } from 'react-redux';
 import App from './App';
-import {register} from './serviceWorker';
+import { register } from './serviceWorker';
 import store from './store';
+import {IonToast} from '@ionic/react';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
     <App />
+    <IonToast isOpen={true} message="Your settings have been saved." />
   </Provider>,
   rootElement
 );
 
-register();
+register({
+  onUpdate: async (registration: ServiceWorkerRegistration) => {
+    await registration.update();
+    console.log(registration);
+    console.log('new version');
+  }
+});
